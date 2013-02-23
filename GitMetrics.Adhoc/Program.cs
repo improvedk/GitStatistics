aff2f6faa5e52fb67df8dbf9cf0d4b8b93e85697
Mark S. Rasmussen <msr@ipaper.dk>
2013-02-23 17:36:30 +0100

    Added GetCommmitCountPerDay and GetCommitCountPerMonth functions

--- GitMetrics.Adhoc/Program.cs
+++ GitMetrics.Adhoc/Program.cs
@@ -1,12 +1,38 @@
-﻿namespace GitMetrics.Adhoc
+﻿using System;
+
+namespace GitMetrics.Adhoc
 {
 	class Program
 	{
+		private const string repoPath = @"E:\iPaperCMS";
+
 		static void Main()
 		{
-			using (var repo = new MetricsRepo(@"E:\OrcaMDF"))
+			printCommitsPerMonth();
+
+			Console.WriteLine("Done");
+			Console.ReadLine();
+		}
+
+		static void printCommitsPerDay()
+		{
+			using (var repo = new MetricsRepo(repoPath))
 			{
-				
+				var result = repo.GetCommitCountPerDay();
+
+				foreach (var date in result.Keys)
+					Console.WriteLine(date + ": " + result[date]);
+			}
+		}
+
+		static void printCommitsPerMonth()
+		{
+			using (var repo = new MetricsRepo(repoPath))
+			{
+				var result = repo.GetCommitCountPerMonth();
+
+				foreach (var date in result.Keys)
+					Console.WriteLine(date + ": " + result[date]);
 			}
 		}
 	}
--- GitMetrics/MetricsRepo.cs
+++ GitMetrics/MetricsRepo.cs
@@ -1,5 +1,6 @@
 ﻿using LibGit2Sharp;
 using System;
+using System.Collections.Generic;
 
 namespace GitMetrics
 {
@@ -10,6 +11,47 @@ namespace GitMetrics
 		public MetricsRepo(string repoPath)
 		{
 			repo = new Repository(repoPath);
+		}
+
+		/// <summary>
+		/// Aggregates all commits and counts the number of commits per day. Days with no commits are not included.
+		/// </summary>
+		public SortedDictionary<DateTime, int> GetCommitCountPerDay()
+		{
+			var result = new SortedDictionary<DateTime, int>();
+
+			foreach (var commit in repo.Commits)
+			{
+				var date = new DateTime(commit.Author.When.Year, commit.Author.When.Month, commit.Author.When.Day);
+				
+				if (!result.ContainsKey(date))
+					result.Add(date, 0);
+
+				result[date]++;
+			}
+
+			return result;
+		}
+
+		/// <summary>
+		/// Aggregates all commits and counts the number of commits per month. Months with no commits are not included.
+		/// </summary>
+		/// <returns></returns>
+		public SortedDictionary<DateTime, int> GetCommitCountPerMonth()
+		{
+			var result = new SortedDictionary<DateTime, int>();
+
+			foreach (var commit in repo.Commits)
+			{
+				var date = new DateTime(commit.Author.When.Year, commit.Author.When.Month, 1);
+
+				if (!result.ContainsKey(date))
+					result.Add(date, 0);
+
+				result[date]++;
+			}
+
+			return result;
 		}
 
 		public void Dispose()
